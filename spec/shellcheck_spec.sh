a82408b4d7d846751f18b7817d29c476e6969097
qq542vev <qq542vev@yahoo.co.jp>
2025-09-09 00:53:18 +0900

    chore: spec内のファイル名を変更。

--- spec/shellcheck_spec.sh
+++ spec/shellcheck_spec.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env sh
 
-### File: html_spec.sh
+### File: sh_spec.sh
 ##
-## *.htmlファイルの検証。
+## *.shファイルの検証。
 ##
 ## Usage:
 ##
 ## ------ Text ------
-## shellspec html_spec.sh
+## shellspec sh_spec.sh
 ## ------------------
 ##
 ## Metadata:
 ##
-##   id - c2c4b805-b5a0-4393-b37e-5943e5bdbb13
+##   id - 8bcfd6aa-7529-421a-b2c3-366f43ffede6
 ##   author - <qq542vev at https://purl.org/meta/me/>
 ##   version - 1.0.0
-##   created - 2025-06-02
-##   modified - 2025-06-02
+##   created - 2025-06-03
+##   modified - 2025-06-03
 ##   copyright - Copyright (C) 2025-2025 qq542vev. All rights reserved.
 ##   license - <GNU GPLv3 at https://www.gnu.org/licenses/gpl-3.0.txt>
-##   depends - find, tidy 
+##   depends - find, sh, shellcheck
 ##
 ## See Also:
 ##
@@ -28,12 +28,17 @@
 
 eval "$(shellspec - -c) exit 1"
 
-Describe '*.htmlファイルの検証'
+Describe '*.shファイルの検証'
 	# shellcheck disable=SC2046
-	Parameters:value 'index.html'
+	Parameters:value $(find spec -name '*.sh' -type f)
 
-	Example "tidy -eq '${1}'"
-		When call tidy -eq -- "${1}"
-		The status should not eq 2
+	Example "sh -n -- '${1}'"
+		When call sh -n -- "${1}"
+		The status should eq 0
+	End
+
+	Example "shellcheck -- '${1}'"
+		When call shellcheck -f gcc -s sh -- "${1}"
+		The status should eq 0
 	End
 End
