d6281e4b32188878e8a72e07fc41713da7c22e49
qq542vev <qq542vev@yahoo.co.jp>
2025-09-11 19:24:17 +0900

    fix: src/shellspec_spec.shが動作しなかったのを修正。

--- spec/shellspec_spec.sh
+++ spec/shellspec_spec.sh
@@ -2,37 +2,50 @@
 
 ### File: spec_spec.sh
 ##
-## *_spec.shの検証。
+## shellspecによる*_spec.shの検証。
 ##
 ## Usage:
 ##
 ## ------ Text ------
-## shellspec spec_spec.sh
+## shellspec shellspec_spec.sh
 ## ------------------
 ##
 ## Metadata:
 ##
-##   id - dc968ca8-2626-44f5-b375-34ecb10fcfdc
+##   id - dc64e0cc-a435-4391-86d9-417a72926e90
 ##   author - <qq542vev at https://purl.org/meta/me/>
 ##   version - 1.0.0
 ##   created - 2025-06-03
-##   modified - 2025-06-03
+##   modified - 2025-09-11
 ##   copyright - Copyright (C) 2025-2025 qq542vev. All rights reserved.
 ##   license - <GNU GPLv3 at https://www.gnu.org/licenses/gpl-3.0.txt>
-##   depends - shellspec
+##   depends - find, shellspec
 ##
 ## See Also:
 ##
-##   * <Project homepage at https://github.com/qq542vev/bookmarklet-generator>
-##   * <Bag report at https://github.com/qq542vev/bookmarklet-generator>
+##   * <Project homepage at https://github.com/qq542vev/sslk>
+##   * <Bag report at https://github.com/qq542vev/sslk/issues>
 
 eval "$(shellspec - -c) exit 1"
 
+set -f
+
 Describe '*_spec.shの検証'
-	Example 'shellspec --syntax-check'
-		When call shellspec --syntax-check
-		The stdout should not eq ''
-		The stderr should eq ''
+	shellspec_test() (
+		# shellcheck disable=SC2016
+		code='
+			IFS="${SSKIT_IFS-${IFS}}"
+
+			shellspec --syntax-check ${SSKIT_MARKDOWNLINT_ARGS-} -- "${@}" >/dev/null
+		'
+		IFS="${SSKIT_IFS-${IFS}}"
+
+		# shellcheck disable=SC2086
+		find . ${SSKIT_FIND_ARGS-} -name '?*_spec.sh' -type f -exec sh -fc "${code}" sh '{}' +
+	)
+
+	Example 'shellspec --syntax-check *_spec.sh'
+		When call shellspec_test
 		The status should eq 0
 	End
 End
