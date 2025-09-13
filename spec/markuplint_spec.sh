e4a1649c14ad629c36acbee9e7844012806bc749
qq542vev <qq542vev@yahoo.co.jp>
2025-09-14 05:04:51 +0900

    feat: markuplint_spec.shを追加。

---
+++ spec/markuplint_spec.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env sh
+
+### File: markuplint_spec.sh
+##
+## markuplintによる*.html, *.xhtmlの検証。
+##
+## Usage:
+##
+## ------ Text ------
+## shellspec markuplint_spec.sh
+## ------------------
+##
+## Metadata:
+##
+##   id - e01e7a20-0c6f-44fc-a602-63f5067430d5 
+##   author - <qq542vev at https://purl.org/meta/me/>
+##   version - 1.0.0
+##   created - 2025-09-14
+##   modified - 2025-09-14
+##   copyright - Copyright (C) 2025-2025 qq542vev. All rights reserved.
+##   license - <GNU GPLv3 at https://www.gnu.org/licenses/gpl-3.0.txt>
+##   depends - find, markuplint 
+##
+## See Also:
+##
+##   * <Project homepage at https://github.com/qq542vev/sslk>
+##   * <Bag report at https://github.com/qq542vev/sslk/issues>
+
+eval "$(shellspec - -c) exit 1"
+
+Include "${SHELLSPEC_HELPERDIR}/sskit.sh"
+
+Describe 'Test: *.html, *xhtml' sskit category:html
+	if [ -z "${SSKIT_MARKUPLINLT_CMD+_}" ]; then
+		Skip if 'not exists markuplint' sskit_not_exists_all markuplint
+	fi
+
+	markuplint_test() {
+		# shellcheck disable=SC2016
+		sskit_find_file '${SSKIT_MARKUPLINLT_CMD:-markuplint} ${SSKIT_MARKUPLINLT_ARGS-} -- "${@}"' '?*.html' '?*.xhtml'
+	}
+
+	Example "markuplint *.html *.xhtml"
+		When call markuplint_test
+		The status should eq 0
+	End
+End
