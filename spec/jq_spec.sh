a5e21a8fead3646c81b02237be89b31cae278c37
qq542vev <qq542vev@yahoo.co.jp>
2025-09-16 03:02:58 +0900

    feat: spec/jq_spec.shを追加。

---
+++ spec/jq_spec.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env sh
+
+### File: jq_spec.sh
+##
+## jqによる*.jsonファイルの検証。
+##
+## Usage:
+##
+## ------ Text ------
+## shellspec jq_spec.sh
+## ------------------
+##
+## Metadata:
+##
+##   id - 471b67fe2c490a129bc8a6de4f5ea614df66752a
+##   author - <qq542vev at https://purl.org/meta/me/>
+##   version - 1.0.0
+##   created - 2025-09-15
+##   modified - 2025-09-15
+##   copyright - Copyright (C) 2025-2025 qq542vev. All rights reserved.
+##   license - <GNU GPLv3 at https://www.gnu.org/licenses/gpl-3.0.txt>
+##   depends - jq
+##
+## See Also:
+##
+##   * <Project homepage at https://github.com/qq542vev/sslk>
+##   * <Bag report at https://github.com/qq542vev/sslk/issues>
+
+eval "$(shellspec - -c) exit 1"
+
+for inc in "${SHELLSPEC_HELPERDIR}/lib/speckit.sh" "${SHELLSPEC_SPECFILE}/../lib/speckit.sh"; do
+	[ -z "${SPECKIT_MODULE_LODAD+_}" ] || break
+
+	if [ -f "${inc}" ]; then
+		Include "${inc}"
+	fi
+done
+
+Describe 'jq' speckit category:json
+	if [ -z "${SPECKIT_JQ_CMD+_}" ]; then
+		Skip if 'not exists jq' speckit_not_exists_all jq
+	fi
+
+	jq_test() {
+		# shellcheck disable=SC2016
+		speckit_find_file '
+			for file in "${@}"; do
+				out=$(${SPECKIT_JQ_CMD:-jq} ${SPECKIT_JQ_ARGS-} empty -- "${file}" 2>&1) || exit="${exit-${?}}"
+
+				[ -n "${out}" ] && printf "%s: %s\\n" "${file}" "${out}" >&2
+			done
+
+			exit "${exit-0}"
+		' '?*.json'
+	}
+
+	Example '*.json'
+		When call jq_test
+		The status should eq 0
+	End
+End
